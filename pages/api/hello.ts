7dd01f1ecb8d98940a72fd34c08df3f98c52e2ff
navin mishra <navinmishra1717@gmail.com>
2022-03-17 13:16:47 +0545

    eslint and prisma

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,29 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import Cors from 'cors';
-
-// Initialize the cors middleware
-const cors = Cors({
-  methods: ['GET', 'HEAD'],
-});
+import type { NextApiRequest, NextApiResponse } from 'next';
+// import Cors from 'cors';
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any): Promise<void> {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (err?: Error) => {
-      if (err) return reject(err)
-      return resolve()
-    })
-  })
-}
+// Initialize the cors middleware
+// const cors = Cors({
+//   methods: ['GET', 'HEAD'],
+// });
+
+// function runMiddleware(
+//   req: NextApiRequest,
+//   res: NextApiResponse,
+//   fn: any
+// ): Promise<void> {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (err?: Error) => {
+//       if (err) return reject(err);
+//       return resolve();
+//     });
+//   });
+// }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  await runMiddleware(req, res, cors);
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' });
 }
---
+++ pages/api/users/index.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const users = await prisma.user.findMany();
+  console.log(users);
+  res.status(200).json(users);
+}
